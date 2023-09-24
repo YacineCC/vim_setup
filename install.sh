b39469269d0b412e3429529dcecda2088bf5ab0a
YacineCC <Haouas.Yacine@hotmail.com>
2023-09-24 17:48:17 +0200

    test d'un script qui fait tout

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash	
+cd ~/;git clone https://github.com/YacineCC/vim_setup.git;
+chmod +x install.sh;
+cp ~/vim_setup/.vimrc ~/vim_setup/.vimrc.plug ~/;	
+mkdir -p ~/.vim ~/.vim/autoload ~/.vim/backup ~/.vim/colors ~/.vim/plugged ~/.vim/plug;
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim;
+vim ~/.vimrc.plug;
+:PlugInstall
+q
+:wq
